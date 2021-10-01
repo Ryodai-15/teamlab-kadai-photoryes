dc7ae2f1a8fcc9dd8776d2c39e8882f9453be491
Ryodai-15 <gon.4629.1208@gmail.com>
2021-10-01 22:20:45 +0000

    [finish]my_page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :post_images, dependent: :destroy
   # 画像投稿とのアソシエーション、　１：N
-         
+
+  attachment :profile_image
+  # モデルに、画像アップ用のメソッド（attachment）を追加してフィールド名に（profile_image）を指定
+
 end
